9197cbb7b554d9c8f15fd72b4481f6b779bd692d
Florian Muellerklein <flo@Florians-MacBook-Pro-2.local>
2025-03-16 20:42:43 -0400

    first version of OS GPS device

--- AutoXDLGPS.ino
+++ AutoXDLGPS.ino
@@ -1,23 +1,6 @@
 /*
-    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
-    updated by chegewara
-
-   Create a BLE server that, once we receive a connection, will send periodic notifications.
-   The service advertises itself as: 4fafc201-1fb5-459e-8fcc-c5c9c331914b
-   And has a characteristic of: beb5483e-36e1-4688-b7f5-ea07361b26a8
-
-   The design of creating the BLE server is:
-   1. Create a BLE Server
-   2. Create a BLE Service
-   3. Create a BLE Characteristic on the Service
-   4. Create a BLE Descriptor on the characteristic
-   5. Start the service.
-   6. Start advertising.
-
-   A connect handler associated with the server starts a background task that performs notification
-   every couple of seconds.
+  Heavily based on the example BLE notify code provided with ESP32 on Arduino IDE. 
+  Modified to use TinyGPSPlus and to send a 20 byte packet from an external GPS module. 
 */
 #include <BLEDevice.h>
 #include <BLEServer.h>
@@ -34,8 +17,7 @@ BLE2901 *descriptor_2901 = NULL;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
+// DO NOT CHANGE
 #define SERVICE_UUID        "ECBB8159-FBA9-4123-AEF0-CA06E1D390D9"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
@@ -118,7 +100,7 @@ void setup() {
   gpsSerial.begin(GPS_BAUD, SERIAL_8N1, RXPin, TXPin);
   Serial.println("GPS Started!");
 
-  // Create the BLE Device
+  // name the GPS device DO NOT CHANGE
   BLEDevice::init("DIYAutoXGPS");
 
   // Create the BLE Server
